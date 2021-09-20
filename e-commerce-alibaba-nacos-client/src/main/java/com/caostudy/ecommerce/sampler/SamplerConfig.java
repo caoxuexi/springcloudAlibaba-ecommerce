1fbcf8e66632c1e267e237472085fc94e4630001
CaoStudy <969718359@qq.com>
2021-09-20 16:10:12 +0800

    链路追踪微服务搭建 zipkin+sleuth

---
+++ e-commerce-alibaba-nacos-client/src/main/java/com/caostudy/ecommerce/sampler/SamplerConfig.java
@@ -0,0 +1,10 @@
+package com.caostudy.ecommerce.sampler;
+
+/**
+ * @author Cao Study
+ * @description <h1>SamplerConfig使用配置的方式设定抽样率</h1>
+ * @date 2021/9/20 16:09
+ */
+public class SamplerConfig {
+
+}
